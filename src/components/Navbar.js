fe5472bf75ed8927671a42ef82301182a2a3df00
tiendp <hoangdangtien1108@gmail.com>
2023-10-30 16:47:44 +0700

    fixed Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
             <div className='bg-white flex justify-between items-center navBar2XL:px-[160px] navBarXl:px-[120px] navBarLg:px-[80px] menuPhone:px-[50px] '>
                 <div className='flex items-center navBar2XL:w-[85%] navBarXl:w-[82%] navBarTablet:w-[82%] w-[100%]'>
                     <div className='flex justify-between items-center navBarTablet:w-auto w-[100%] navBarTablet:gap-0 menuPhone:gap-[44px] navBarTablet:p-0 menuPhone::px-[16px] py-[16px] px-[20px] gap-0'>
-                        <img className='opacity-0' src='images/menu.png' alt=''/>
+                        <img className='navBarTablet:hidden flex opacity-0' src='images/menu.png' alt=''/>
                         <img src='images/logo.png' alt=''/>
                         <img className='navBarTablet:hidden flex' src='images/menu.png' alt=''/>
                     </div>
